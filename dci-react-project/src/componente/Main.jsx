23a59b66040439d0e5881c64063ac61c8c6f3f99
Chaz1q <c1x.chaz1q@gmail.com>
2024-04-11 12:06:56 +0200

    + icon + text align

--- dci-react-project/src/componente/Main.jsx
+++ dci-react-project/src/componente/Main.jsx
@@ -1,8 +1,8 @@
 import { Carousel } from "@material-tailwind/react";
- 
+
 export default function Main() {
   return (
-    <Carousel style={{height:"93.4 vh"}}>
+    <Carousel style={{ height: "93.4 vh" }}>
       <img
         src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
         alt="image 1"
--- dci-react-project/src/componente/Navbar.jsx
+++ dci-react-project/src/componente/Navbar.jsx
@@ -3,10 +3,10 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  // { name: 'Dashboard', href: '#', current: true },
+  { name: 'Shop', href: '#', current: false },
+  { name: 'About', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -36,18 +36,18 @@ export default function Navbar() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src="https://www.pngall.com/wp-content/uploads/5/Laboratory-Flask-Glass-PNG-Picture.png"
+                    alt="LevelUp"
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                  <div className="flex space-x-4 items-center">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-green-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -55,15 +55,15 @@ export default function Navbar() {
                         {item.name}
                       </a>
                     ))}
- <div className="relative mr-6 my-2">
-  <input  type="search" className="bg-purple-white shadow rounded border-0 p-0.5 " placeholder="Search by name..."/>
-  <div className="absolute pin-r pin-t text-purple-lighter">
-  	
+                    <div className="relative mr-6 my-2">
+                      <input type="search" className="text-center bg-purple-white shadow rounded border-0 p-0.5" placeholder="Search for game" />
+                      <div className="absolute pin-r pin-t text-purple-lighter">
 
-  </div>
-</div>
+
+                      </div>
+                    </div>
                   </div>
-                 
+
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -135,7 +135,7 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-{/* input search */}
+          {/* input search */}
 
 
 
